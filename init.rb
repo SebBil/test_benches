55bb6ec4334877c63c9b46b73b8ab87e3adc2473
Sebastian Bilda <sebastian.bilda@outlook.de>
2021-03-16 11:50:37 +0100

    update version

--- init.rb
+++ init.rb
@@ -1,13 +1,14 @@
 Redmine::Plugin.register :test_benches do
   name 'Test Benches plugin'
   author 'Sebastian Bilda'
-  description 'This is a plugin for Redmine.'
-  version '0.0.1'
+  description 'Add Testbenches for an overview what is inside an Testbench. For every Project you can assign some Testbenches. If you create in subproject Testbenches, these were shown
+  in the main project for an overview, but not vice versa.'
+  version '0.0.2'
 
   project_module :benches do
     permission :view_benches, { benches: [:index, :show_bench] }
     permission :edit_benches, { benches: [:new_bench, :create_bench, :edit_bench, :update_bench, :delete_bench] }
-    permission :ecus, { ecus: [:create_ecu, :new_ecu, :delete_ecu, :edit_ecu, :update_ecu]}
+    permission :edit_ecus, { ecus: [:create_ecu, :new_ecu, :delete_ecu, :edit_ecu, :update_ecu]}
   end
 
   menu :project_menu, :benches, { controller: 'benches', action: 'index' }, caption: 'Test Benches'
